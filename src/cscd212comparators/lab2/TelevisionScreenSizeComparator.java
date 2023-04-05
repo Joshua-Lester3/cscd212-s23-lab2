b92a99e7f87ea2773e9aad65df9bc868afce2d70
Joshua Lester <jlester3@ewu.edu>
2023-04-05 13:47:25 -0700

    Added third and final comparator

---
+++ src/cscd212comparators/lab2/TelevisionScreenSizeComparator.java
@@ -0,0 +1,11 @@
+package cscd212comparators.lab2;
+
+import cscd212classes.lab2.Television;
+
+import java.util.Comparator;
+
+public class TelevisionScreenSizeComparator implements Comparator<Television> {
+    public int compare(final Television t1, final Television t2) {
+        return Integer.compare(t1.getScreenSize(), t2.getScreenSize());
+    }
+}
